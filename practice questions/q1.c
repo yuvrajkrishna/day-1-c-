bf6aba59ff08f656aba5d97ceab764eefa60e348
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-05-12 20:49:35 +0530

    second commit

--- practice questions/q1.c
+++ practice questions/q1.c
@@ -2,12 +2,19 @@
 
 int main (){
     int area_rectangle,length,width;
-    printf("Enter the area of the length \n");
-    scanf("%d",&length);
-    printf("Enter the area of the width \n");
-    scanf("%d",&width);
+    length=5;   
+    width=5;
     area_rectangle= length*width;
-    printf("The area of the rectangle is %d",area_rectangle);
+    printf("The area of the rectangle is %d \n",area_rectangle);
+
+
+    int inputed_area_rectangle,inputed_length,inputed_width;
+    printf("Enter the length \n");
+    scanf("%d",&inputed_length);
+    printf("Enter the width \n");
+    scanf("%d",&inputed_width);
+    inputed_area_rectangle= inputed_length*inputed_width;
+    printf("The area of the rectangle is %d",inputed_area_rectangle);
 
     return 0;
 }
